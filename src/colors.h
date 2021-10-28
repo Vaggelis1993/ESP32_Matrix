bb825d96741f722073a434197d6ffb46c7ec279e
Vaggelis_Myl <v.mylonas1993@gmail.com>
2021-10-28 22:22:12 +0300

    Add files via upload

---
+++ src/colors.h
@@ -0,0 +1,20 @@
+//Colors
+int rtime = 7;
+int gtime = 0;
+int btime = 0;
+
+int rdate = 0;
+int gdate = 7;
+int bdate = 3;
+
+int rtemp = 0;
+int gtemp = 0;
+int btemp = 7;
+
+int rtempin = 7;
+int gtempin = 7;
+int btempin = 0;
+
+int rtext = 7;
+int gtext = 7;
+int btext = 7;
