6e08f605e9e6ab0437299a0ac8046b50a8cf0e7c
Vaggelis_Mylonas <v.mylonas1993@gmail.com>
2021-11-03 11:34:00 +0200

    Add files via upload

---
+++ src/colors.h
@@ -0,0 +1,26 @@
+//Colors on rgb code (0-7)
+
+//Color setting for time
+int rtime = 7;
+int gtime = 0;
+int btime = 0;
+
+//Color setting for date
+int rdate = 0;
+int gdate = 7;
+int bdate = 3;
+
+//Color setting for external temperature
+int rtemp = 0;
+int gtemp = 0;
+int btemp = 7;
+
+//Color setting for internal temperature
+int rtempin = 7;
+int gtempin = 7;
+int btempin = 0;
+
+//Color setting for scrolling text
+int rtext = 7;
+int gtext = 7;
+int btext = 7;
